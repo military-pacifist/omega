550621eb60a774ebb2994e7620cb537c8f77f5e5
military-pacifist <sasamelkinanc@gmail.com>
2025-08-05 05:21:05 +0200

    feat: added music player

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const albumWrappers = document.querySelectorAll('.album-wrapper');
+    const trackLists = document.querySelectorAll('.track-list');
 
-function toggleTracks(wrapper) {
-    const trackList = wrapper.nextElementSibling;
-    if (trackList && trackList.classList.contains('track-list')) {
-        trackList.classList.toggle('hidden');
+    albumWrappers.forEach(wrapper => {
+        wrapper.addEventListener('click', function() {
+            const albumId = this.dataset.albumId
+            showTracks(albumId)
+        });
+    });
+
+    function showTracks(albumId) {
+        albumWrappers.forEach(wrapper => {
+            wrapper.classList.remove('active');
+        });
+
+        trackLists.forEach(list => {
+            list.classList.remove('active');
+        });
+
+        const selectedAlbum = document.querySelector(
+            `.album-wrapper[data-album-id="${albumId}"]`
+        );
+        const selectedTrackList = document.querySelector(
+            `.track-list[data-album-id="${albumId}"]`
+        );
+
+        if (selectedTrackList) {
+            selectedTrackList.classList.add('active');
+        }
+        if (selectedAlbum) {
+            selectedAlbum.classList.add('active');
+        }
     }
-}
+});
