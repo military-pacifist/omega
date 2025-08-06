c7ea73dd5c26b7456b4992e18394dee48541c56a
OrtiMorti <yourmama@mail.ru>
2025-08-07 00:09:06 +0200

    feat: fucked up footer

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,13 @@ function toggleTracks(wrapper) {
         trackList.classList.toggle('hidden');
     }
 }
+
+const faqItems = document.querySelectorAll('.faq_item');
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq_question');
+
+  question.addEventListener('click', () => {
+    faqItems.forEach(i => i.classList.remove('active')); // Закрыть все ответы
+    item.classList.add('active'); // Открыть текущий
+  });
+});
